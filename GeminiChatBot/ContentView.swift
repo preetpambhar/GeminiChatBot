aa9a91412f5d53770eac16eb3d53cbacfe863d9d
Preet Pambhar <preetpambhar73530@gmail.com>
2024-06-28 23:52:12 -0400

    Initial Commit

---
+++ GeminiChatBot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeminiChatBot
+//
+//  Created by Preet Pambhar on 2024-06-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeminiChatBot/GeminiChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiChatBotApp.swift
+//  GeminiChatBot
+//
+//  Created by Preet Pambhar on 2024-06-28.
+//
+
+import SwiftUI
+
+@main
+struct GeminiChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
