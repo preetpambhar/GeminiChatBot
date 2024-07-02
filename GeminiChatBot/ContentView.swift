20a232efc0c9ae319c02e27f6914f8852994c67a
Preet Pambhar <preetpambhar73530@gmail.com>
2024-07-02 00:41:38 -0400

    feat: fetch response

--- GeminiChatBot/ContentView.swift
+++ GeminiChatBot/ContentView.swift
@@ -6,16 +6,61 @@
 //
 
 import SwiftUI
+import GoogleGenerativeAI
 
 struct ContentView: View {
+    let model = GenerativeModel(name: "gemini-pro", apiKey: APIKey.default)
+    
+    @State var userPrompt = ""
+    @State var response: LocalizedStringKey = "How can i help you today?"
+    @State var loding = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Text("Welcome To Genimi AI")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.indigo)
+                .padding(.top, 40)
+            
+            ZStack{
+                ScrollView{
+                    Text(response)
+                        .font(.title)
+                }
+                if loding{
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: .indigo))
+                        .scaleEffect(2)
+                    
+                }
+            }
+            TextField("Ask anything...", text: $userPrompt, axis: .vertical)
+                .lineLimit(5)
+                .font(.title)
+                .padding()
+                .background(Color.indigo.opacity(0.2), in: Capsule())
+                .disableAutocorrection(true)
+                .onSubmit {
+                    generateResponse()
+                }
         }
+        
         .padding()
+    }
+    func generateResponse(){
+        loding = true
+        response = ""
+        
+        Task {
+            do {
+                let result = try await model.generateContent(userPrompt)
+                loding = false
+                response = LocalizedStringKey(result.text ?? "No Response Found")
+                userPrompt = ""
+            }catch {
+                response = "Something Went Wrong\n\(error.localizedDescription)"
+            }
+        }
     }
 }
 
